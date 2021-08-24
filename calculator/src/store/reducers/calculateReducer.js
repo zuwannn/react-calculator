540ab8eed3ae1831379ac5d7a4169c03a7f10652
zuwannn <suwanneethongtha@gmail.com>
2021-08-25 01:05:43 +0700

    Update calculateReducer.js

--- calculator/src/store/reducers/calculateReducer.js
+++ calculator/src/store/reducers/calculateReducer.js
@@ -6,16 +6,58 @@ let initialState = {
     total: 0
 }
 
+function setExpression({ expression, total}, action){
+    if(
+        /[\d]*[-+%*/.]$/.exec(expression) &&
+        /[-+%*/.]/.exec(action.payload) 
+    ){
+        console.log('b', expression);
+        expression = expression.slice(0, expression.length - 1 )
+        console.log('a', expression);
+    }
+
+    switch(action.type){
+        case types.SET_EXPRESSION:
+            if(['+', '/', '*', '%'].includes(action.payload) && !expression){
+                return `${total}${action.payload}`
+            }
+            return `${!expression && total ? total : ''}${expression + action.payload}`
+        default:
+            return expression
+    }
+}
+
 export default (state = initialState, action) => {
     switch (action.type) {
         case types.SET_EXPRESSION:
-            let expression = state.expression + action.payload
+            let expression = setExpression(state, action)
             return{
                 ...state,
                 expression,
-                total: calculate(expression)
+                total: calculate(expression) || state.total
             }
-    
+        case types.CLEAR_EXPRESSION:
+            return {
+                ...state, 
+                expression: exp,
+                total: calculate(exp)
+            }
+
+        case types.DELETE_LAST_EXPRESSION_ENTRY:
+            let exp = state.expression
+            exp = exp.split('').slice(0, exp.length - 1).join('')
+            return {
+                ...state,
+                expression: exp,
+                total: calculate(exp)
+            }
+            
+        case types.EVALUATE_EXPRESSION:
+            return {
+                ...state,
+                expression: '',
+                total: calculate(state.expression) || state.expression || state.total
+            }
         default:
             return state
     }
